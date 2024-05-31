a0c9e0ac892f437f7e07d4e1aecfd9fe5683d67a
Cheng-Zhang <2694824893@qq.com>
2024-05-31 14:03:01 +0800

    feat: kubelet join

--- cmd/kubelet/kubelet.go
+++ cmd/kubelet/kubelet.go
@@ -1,9 +1,31 @@
 package main
 
-import "minikubernetes/pkg/kubelet/app"
+import (
+	"fmt"
+	"minikubernetes/pkg/kubelet/app"
+	"net"
+	"os"
+)
+
+func usage() {
+	fmt.Println("usage: kubelet -j|--join <apiServerIP>")
+	os.Exit(1)
+}
 
 func main() {
-	kubeletServer, err := app.NewKubeletServer("10.119.12.123")
+	if len(os.Args) != 3 {
+		usage()
+	}
+	if os.Args[1] != "-j" && os.Args[1] != "--join" {
+		usage()
+	}
+	ip := os.Args[2]
+	if net.ParseIP(ip) == nil {
+		fmt.Printf("invalid ip: %s\n", ip)
+		os.Exit(1)
+	}
+	kubeletServer, err := app.NewKubeletServer(ip)
+	// kubeletServer, err := app.NewKubeletServer("10.119.12.123")
 	if err != nil {
 		return
 	}
